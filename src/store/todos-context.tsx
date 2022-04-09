0ad11cd590a232f3fc52cb20e86241dac49147c0
Carlos Avendaño <46610962+carlospsl10@users.noreply.github.com>
2022-04-09 17:58:06 -0500

    first commit

---
+++ src/store/todos-context.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Todo from "../models/todo";
+
+type todosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<todosContextObj>({
+  items: [],
+  addTodo: (text: string) => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC<{ children: React.ReactNode }> = (
+  props
+) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((todos) => todos.concat(newTodo));
+  };
+
+  const removeTodoHandler = (id: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const contextValue: todosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
